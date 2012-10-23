3d785c2997cf6642d0f96d85eb745ee5c9f64baa
wqli78 <wqli78@gmail.com>
2012-10-23 09:09:20 +0800

    ccc

--- controllers.js
+++ controllers.js
@@ -4,33 +4,47 @@
 
 function IndexCtrl($scope, $http, Questions) {
 
-	$scope.number = 0;
-	$scope.grade = Questions.get({}, function(grade) {
+	myInit();
+
+	$scope.grade = Questions.get({}, function(grade) { //题库对象
 		// console.log(questions.questions);
 		$scope.currentQuestion = $scope.grade.questions[$scope.number];
 		$scope.total = $scope.grade.questions.length;
 	});
 
-	$scope.score = 0;
 
+	//点击选项后的动作
 	$scope.selectAnswer = function(answer) {
 		if($scope.currentQuestion.answer == answer) {
-			$scope.score = $scope.score + 10; 
-			$("#right").show(function(){
+			$scope.score = $scope.score + 10;
+			$scope.rightNumber++;
+			$("#right").show(function() {
 				$("#right").fadeOut(2000);
 			});
 			// alert("恭喜您，答对了，加10分！"); 
 		} else {
-			$("#error").show(function(){
+			$("#error").show(function() {
 				$("#error").fadeOut(2000);
 			});
 			// alert("很遗憾，答错了！");
 		}
-		if (($scope.number + 1) < $scope.grade.questions.length){
-				$scope.number++;
-				$scope.currentQuestion = $scope.grade.questions[$scope.number];
+		if(($scope.number + 1) < $scope.grade.questions.length) {
+			$scope.number++;
+			$scope.currentQuestion = $scope.grade.questions[$scope.number];
+		} else {
+			$.mobile.changePage($('#answerResult'));
 		}
 	};
 
+	//重新开始答题
+	$scope.restart = function() {
+		myInit();
+		$.mobile.changePage($('#answerQuestion'));
+	}
 
+	function myInit() {
+		$scope.number = 0; //答到第几道题
+		$scope.rightNumber = 0; //答对了几道题
+		$scope.score = 0; //得分		
+	}
 }
