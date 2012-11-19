7839d3ce4b856ae2cc66f1914a7f6247433f75b2
wqli78 <wqli78@gmail.com>
2012-11-20 01:12:48 +0800

    cc

--- controllers.js
+++ controllers.js
@@ -7,47 +7,61 @@ function IndexCtrl($scope, $http, Questions) {
 	//选择考试阶段
 	$scope.selectLevel = function(level) {
 
-		myInit();		
-		var gradeName="youeryuan";
+		myInit();
+		var gradeName = "youeryuan";
 		if(level == 1) {
 			gradeName = "youeryuan";
 		} else if(level == 2) {
 			gradeName = "xiaoxue";
 		}
-		$scope.grade = Questions.get({gradeName:gradeName}, function(grade) { //题库对象
+		$scope.grade = Questions.get({
+			gradeName: gradeName
+		}, function(grade) { //题库对象
 			// console.log($scope.grade);
 			$scope.currentQuestion = $scope.grade.questions[$scope.number];
 			$scope.total = $scope.grade.questions.length;
 		});
 
-		$.mobile.changePage($('#answerQuestion'), { transition: "slide"} );
+		$.mobile.changePage($('#answerQuestion'), {
+			transition: "slide"
+		});
 
 	};
 
 	//选择答案
 	$scope.selectAnswer = function(answer) {
 		if($scope.currentQuestion.answer == answer) {
-			$("#right").show(function() {
-				$("#right").fadeOut(1000,function(){
-				});
-			});
+			// $("#right").show(function() {
+			// 	$("#right").fadeOut(1000, function() {});
+			// });
 			$scope.score = $scope.score + 10;
 			$scope.rightNumber++;
+			$("#answerOk").popup("open");
+			setTimeout(function() {
+				$("#answerOk").popup("close");
+				$scope.nextQuestion();
+			}, 1000);
 		} else {
-			$("#error").show(function() {
-				$("#error").fadeOut(1000);
-			});
+			$("#answerError").popup("open");
+			setTimeout(function() {
+				$("#answerError").popup("close");
+				$scope.nextQuestion();
+			}, 1000);;
 		}
-		$scope.nextQuestion();
+
+
 	};
 
-	$scope.nextQuestion = function(){
+	$scope.nextQuestion = function() {
 		if(($scope.number + 1) < $scope.grade.questions.length) {
 			$scope.number++;
 			$scope.currentQuestion = $scope.grade.questions[$scope.number];
 		} else {
 			$.mobile.changePage($('#answerResult'));
+
 		}
+		//将此时的变量状态显式通知html模版进行更新
+		$scope.$apply(function(){   });
 	};
 
 	//重新开始答题
